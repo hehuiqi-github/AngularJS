f5cdb1aed95e4988638611e46d3d8e9b40553097
iMac <hehuiqi@HeiMac.local>
2015-07-24 15:27:51 +0800

    修改错误

--- AgularJSPractice/javaScripts/formModel.js
+++ AgularJSPractice/javaScripts/formModel.js
@@ -1,24 +1,33 @@
 /**
  * Created by hehuiqi on 15/7/24.
  */
-var app =angular.module('MyApp2',[]);
-//自定义过滤器
-
-//app.controller = function($scope)
+//var app =angular.module('MyApp1', []);
+//
+//app.controller("formModel",function($scope)
 //{
 //    $scope.book =
 //    {
-//        name:"javaScript",
+//        bookName:"javaScript",
 //        price:20
 //    };
-//}
-app.filter('capitalize',function()
+//});
+////自定义过滤器
+//app.filter('capitalize',function()
+//{
+//    return function(input)
+//    {
+//        if(input)
+//        {
+//            return input[0].toUpperCase()+input.slice(1);
+//        }
+//    };
+//});
+
+function formModel($scope)
 {
-    return function(input)
+    $scope.book =
     {
-        if(input)
-        {
-            return input[0].toUpperCase()+input.slice(1);
-        }
+        bookName:"javaScript",
+        price:20
     };
-});
+}
